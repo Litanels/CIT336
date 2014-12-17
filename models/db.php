6280f76cc9db17eaa09e54a3eb3d1eba27c0c934
Litanels <litanels@gmail.com>
2014-12-16 22:06:43 -0600

    get database linked correctly

--- models/db.php
+++ models/db.php
@@ -22,4 +22,74 @@ function DbExecute($query, $params = null)
         error_log($e);
         exit();
     }
+    return $statement->rowCount();
+}
+/// Runs an INSERT statement
+/// Returns the ID of the inserted item.
+/// $query - the sql statement to run.
+/// $params (array) - the parameters to bind to the query.
+function DbInsert($query, $params = null)
+{
+    global $db;
+    $return = false;
+    
+    try
+    {
+        $statement = $db->prepare($query);
+        
+        if (is_array($params))
+        {
+            foreach ($params as $key => $value)
+            {
+                $statement->bindValue($key, $value);
+            }
+        }
+        
+        $statement->Execute();
+        $return = $db->lastInsertId();
+    }
+    catch (Exception $e)
+    {
+        echo "Database insert exception, please try again later.";
+        error_log($e);
+        exit();
+    }
+    
+    return $return;
+}
+/// Runs a SELECT statement
+/// Returns an array containing the matching rows.
+/// $query - the sql statement to run.
+/// $params (array) - the parameters to bind to the query.
+function DbSelect($query, $params = null)
+{
+    global $db;
+    $return = array();
+    try 
+    {
+        $statement = $db->prepare($query);
+        
+        if (is_array($params))
+        {
+            foreach ($params as $key => $value)
+            {
+                $statement->bindValue($key, $value);
+            }
+        }
+        
+        $statement->Execute();
+        
+        while ($row = $statement->fetch())
+        {
+            $return[] = $row;
+        }
+    }
+    catch (Exception $e)
+    {
+        echo "Database exception, try again later.";
+        error_log($e);
+        exit();
+    }
+    
+    return $return;
 }
