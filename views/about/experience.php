f2e7cf7c16ad0ab3fa673331bd8a8e609141287f
Litanels <litanels@gmail.com>
2014-12-16 20:20:20 -0600

    updated header

--- views/about/experience.php
+++ views/about/experience.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html> 
-<html> 
-    <head> 
-        <meta charset="utf-8"> 
-        <title>Laura Nelson Design</title> 
-        <link href="/css/style_1.css" type="text/css" rel="stylesheet"/>
-    </head> 
-    <body> 
-       
-           
-
-            <main>
+<main>
                 <div id="main_content">
                     <h1>Skills and Experience</h1>
                     <h2>Programming and Database Experience</h2>
