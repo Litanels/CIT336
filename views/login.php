a67d1daa38a0f2fc54fbeb82ffa9d8b6eff5f922
Litanels <litanels@gmail.com>
2014-12-16 20:21:12 -0600

    updated header

--- views/login.php
+++ views/login.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html> 
-<html>
-    <head> 
-        <meta charset="utf-8"> 
-        <title>Laura Nelson Design</title> 
-        <link href="/css/style_1.css" type="text/css" rel="stylesheet"/>
-        <script src="/js/login.js" ></script>
-    </head> 
-    <body> 
-
-
         <main>
             <div id='main_content'>
                 <h2>Sign-up/Login</h2>
