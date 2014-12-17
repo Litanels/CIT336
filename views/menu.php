c6b4f2ddae30b41c4d72141aa876d4ee745ca4c5
Litanels <litanels@gmail.com>
2014-12-16 20:21:18 -0600

    updated header

--- views/menu.php
+++ views/menu.php
@@ -1,35 +1,24 @@
-
-<!DOCTYPE html> 
-<html> 
-    <head> 
-        <meta charset="utf-8"> 
-        <title>Laura Nelson Design</title> 
-        <link href="/css/style_1.css" type="text/css" rel="stylesheet" media="screen">
-
-        <meta name="viewport" content="width=device-width,initial-scale=1.0, maximum-scale=1.0">
-    </head> 
-    <body> 
         
-            <main>
-<ul>
-	<li><a href="/?action=myinfo">Update my info</a></li>
-	<li><a href="/?action=newitem">Upload a new item</a></li>
-</ul>
+<main>
+    <ul>
+        <li><a href="/?action=myinfo">Update my info</a></li>
+        <li><a href="/?action=newitem">Upload a new item</a></li>
+    </ul>
 
 
-<?php if(LoggedInUserIsAdmin()) : ?>
+    <?php if (LoggedInUserIsAdmin()) : ?>
 
-	Admin Items:<br />
-	<ul>
-		<li><a href="/?action=editusers">Edit Users</a></li>
-                <li><a href="/?action=newitem">Upload a new item</a></li>
-	</ul>
+        Admin Items:<br />
+        <ul>
+            <li><a href="/?action=editusers">Edit Users</a></li>
+            <li><a href="/?action=newitem">Upload a new item</a></li>
+        </ul>
 
-<?php endif; ?>
-            </main>
-         <footer>
-                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modular/footer.php'; ?> 
-            </footer>
-    </body>
+    <?php endif; ?>
+</main>
+<footer>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/modular/footer.php'; ?> 
+</footer>
+</body>
 </html>
-		
+
