ee4a864fe4398f7f8d4dd819138b70abf65655f6
Litanels <litanels@gmail.com>
2014-12-16 20:20:30 -0600

    updated header

--- views/errors/db_error.php
+++ views/errors/db_error.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html> 
-<html> 
-    <head> 
-        <meta charset="utf-8"> 
-        <title>Laura Nelson Design</title> 
-        <link href="/css/style_1.css" type="text/css" rel="stylesheet"/>
-    </head> 
-    <body> 
-       
-           
-            <main>
+<main>
                 <div id='main_content'>
             <h2>Database Error</h2>
             <p>There was an error connecting to the database.</p>
