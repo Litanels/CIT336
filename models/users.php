dcf285ff3de62953a2efece6aa7e160e77cbbb96
Litanels <litanels@gmail.com>
2014-12-17 21:38:35 -0600

    Add javascript link to editusers.php

--- models/users.php
+++ models/users.php
@@ -164,7 +164,7 @@ function ValidateEmail($email, &$message) {
 	return false;
 }
 function ValidatePasswordLength($name, &$message) {
-	// TODO: make sure password contains UPPERCASE, lowercase, numbers, and special chars.
+	
 	
 	if (strlen($name) >= 5) {
 		return true;	
